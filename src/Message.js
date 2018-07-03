5a3b13f02167fa19b92b291890c68ec1444abac4
Arno Saine <arno@mowhi.com>
2018-07-03 13:50:50 +0300

    Delete messages.

--- src/Message.js
+++ src/Message.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import { Alert } from 'react-bootstrap';
+import { dbAddress } from './db';
 
-const Message = ({ message /*,_id, _rev*/ }) => <Alert>{message}</Alert>;
+const Message = ({ message, _id, _rev }) => (
+  <Alert
+    onDismiss={() =>
+      fetch(`${dbAddress}/${_id}?rev=${_rev}`, {
+        credentials: 'same-origin',
+        method: 'DELETE'
+      })
+    }
+  >
+    {message}
+  </Alert>
+);
 
 export default Message;
